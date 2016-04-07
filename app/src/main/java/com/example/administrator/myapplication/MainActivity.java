e0f13af8e7c472cdff163b325cdd5be4e02ebcbb
Administrator <y20110114@163.com>
2016-04-07 23:37:12 +0800

    add TAG；

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.myapplication;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import java.io.FileInputStream;
@@ -10,10 +11,12 @@ import java.io.FileNotFoundException;
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private Button button;
+    private static  String  TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"YANAKI");
         try {
             FileInputStream fileInputStream = new FileInputStream("yankai.txt");
         } catch (FileNotFoundException e) {
