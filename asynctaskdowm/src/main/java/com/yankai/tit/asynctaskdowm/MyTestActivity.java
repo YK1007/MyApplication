73961e186b81261fc6a05b9d269a75f01aab0c66
yankai <y20110114@163.com>
2016-06-07 10:52:07 +0800

    初始化成功。

---
+++ asynctaskdowm/src/main/java/com/yankai/tit/asynctaskdowm/MyTestActivity.java
@@ -0,0 +1,16 @@
+package com.yankai.tit.asynctaskdowm;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/6/7.
+ */
+public class MyTestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
+}
